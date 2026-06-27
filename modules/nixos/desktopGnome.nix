b2ad2195a89d447eef410c2867963aed9cc5d5e2
Deizuu <120480615+Deizuu@users.noreply.github.com>
2026-06-27 12:44:17 +0200

    Disabled Wayland for GDM

--- modules/nixos/desktopGnome.nix
+++ modules/nixos/desktopGnome.nix
@@ -2,7 +2,9 @@
 {
   services.xserver.enable = true;
 
-  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.displayManager.gdm = {
+    enable = true;
+	wayland = false;
   services.xserver.desktopManager.gnome.enable = true;
 
   services.xserver.xkb = {
