0ab13e85201ee2598980e10d778466629e86eac6
Deizuu <120480615+Deizuu@users.noreply.github.com>
2026-06-28 01:09:54 +0200

    Changed modules/nixos/boot.nix and modules/nixos/nvidiaKepler.nix to be flake-parts modules, and downgraded kernel to allow the NVIDIA Kepler driver to work

--- modules/nixos/boot.nix
+++ modules/nixos/boot.nix
@@ -1,16 +1,18 @@
-{ pkgs, ... }:
+{ self, inputs, ... }:
 {
-  boot.loader.grub = {
-    enable = true;
-    device = "nodev"; # Use EFI partition (automatically detected from /boot mount)
-    efiSupport = true;
-    useOSProber = true;
-  };
-  # Note: for non-UEFI systems, change boot.loader to:
-  # "device" to your device (example: /dev/sdX ; /dev/nvme0nX)
-  # "efiSupport" to false
+  flake.nixosModules.boot = { pkgs, ... }: {
+    boot.loader.grub = {
+      enable = true;
+      device = "nodev"; # Use EFI partition (automatically detected from /boot mount)
+	  efiSupport = true;
+      useOSProber = true;
+    };
+    # Note: for non-UEFI systems, change boot.loader to:
+    # "device" to your device (example: /dev/sdX ; /dev/nvme0nX)
+    # "efiSupport" to false
 
-  boot.loader.efi.canTouchEfiVariables = true;
+    boot.loader.efi.canTouchEfiVariables = true;
   
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+    boot.kernelPackages = pkgs.linuxPackages_6_6;
+  };
 }
--- modules/nixos/nvidiaKepler.nix
+++ modules/nixos/nvidiaKepler.nix
@@ -1,18 +1,19 @@
-{ config, pkgs, ... }: {
+{ self, inputs, ... }: {
+  flake.nixosModules.nvidiaKepler = { config, pkgs, ... }: {
 	# Enable X server with NVIDIA driver (Kepler pretty much only supports X11)
 	services.xserver.enable = true;
 	services.xserver.videoDrivers = [ "nvidia" ];
 
 	# NVIDIA driver configuration
 	hardware.nvidia = {
-		enabled = true;
-		package = pkgs.linuxKernel.packages.linux_7_1.nvidia_x11_legacy470;
-		modesetting.enable = true;
-		powerManagement.enable = true;
-		open = false; # Use proprietary driver for better support on older cards
+	  enabled = true;
+	  package = pkgs.linuxKernel.packages.linux_6_6.nvidia_x11_legacy470;
+	  modesetting.enable = true;
+	  powerManagement.enable = true;
+	  open = false; # Use proprietary driver for better support on older cards
 	};
 
 	boot.kernelModules = [ "nvidia" ];
 	boot.kernelParams = [ "nvidia-drm.modeset=1" ];
-
+  };
 }
