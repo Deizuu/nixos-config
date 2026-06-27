9679a980d1dec215c251e36bfd96b91d9171913a
Deizuu <120480615+Deizuu@users.noreply.github.com>
2026-06-27 12:48:40 +0200

    Removed services.xserver.displayManager.gdm.wayland as it does not work

--- modules/nixos/desktopGnome.nix
+++ modules/nixos/desktopGnome.nix
@@ -2,10 +2,7 @@
 {
   services.xserver.enable = true;
 
-  services.xserver.displayManager.gdm = {
-    enable = true;
-	wayland = false;
-  };
+  services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
 
   services.xserver.xkb = {
