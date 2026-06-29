052e1be49e7492dae1dcc8d925e309a35d425918
Deizuu <120480615+Deizuu@users.noreply.github.com>
2026-06-29 17:15:29 +0200

    Updated flake.nix to work with the new structure

--- flake.nix
+++ flake.nix
@@ -19,10 +19,5 @@
   };
 
   outputs = inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = [
-        inputs.home-manager.flakeModules.home-manager
-        (inputs.import-tree [ ./modules ./hosts ])
-      ];
-    };
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree [ ./modules ./hosts ]);
 }
