2df4fe68dbc12f80b098cdcadb809835309304ea
Deizuu <120480615+Deizuu@users.noreply.github.com>
2026-06-29 19:46:22 +0200

    Fixed syntax error

--- hosts/laptopHardware.nix
+++ hosts/laptopHardware.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }:
+{ inputs, ... }: {
   flake.nixosModules.laptopHardware = { config, lib, pkgs, modulesPath, ... }: {
     imports =
     [ (modulesPath + "/installer/scan/not-detected.nix")
@@ -40,4 +40,5 @@
 
     nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
     hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  };
 }
