da8aca704e5c0a1a4f834a1fb9ecb9f9ac2b066e
Deizuu <120480615+Deizuu@users.noreply.github.com>
2026-07-05 18:48:21 +0000

    Changed system.replaceRuntimeDependencies to system.replaceDependencies.replacements

--- modules/system/chromebookAudio.nix
+++ modules/system/chromebookAudio.nix
@@ -43,7 +43,7 @@
       };
     };
 
-    system.replaceRuntimeDependencies = [
+    system.replaceDependencies.replacements = [
       ({
         original = pkgs.alsa-ucm-conf;
 	replacement = cb-ucm-conf;
