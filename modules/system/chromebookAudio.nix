ca3c59e7ce69693f9ad0ff6cc696961dd150be8b
Deizuu <120480615+Deizuu@users.noreply.github.com>
2026-07-05 17:55:45 +0000

    Added chromebookAudio module for fixing audio on granta

---
+++ modules/system/chromebookAudio.nix
@@ -0,0 +1,30 @@
+{ self, inputs, ... }: {
+  flake.nixosModules.chromebookAudio = { config, pkgs, lib, ... }:
+  let
+    cb-ucm-conf = with pkgs; alsa-ucm-conf.overrideAttrs {
+      wttsrc = fetchFromGitHub {
+        owner = "WeirdTreeThing";
+	repo = "alsa-ucm-conf-cros";
+	rev = "6b395ae73ac63407d8a9892fe1290f191eb0315b";
+        hash = "sha256-GHrK85DmiYF6FhEJlYJWy6aP9wtHFKkTohqt114TluI=";
+      };
+      unpackPhase = ''
+        runHook preUnpack
+	tar xf "$src"
+	runHook postUnpack
+      '';
+      installPhase = ''
+        runHook preInstall
+	mkdir -p $out/share/alsa
+	cp -r alsa-ucm*/ucm2 $out/share/alsa
+	runHook postInstall
+      '';
+    };
+  in
+  {
+    environment = {
+      systemPackages = [ pkgs.sof-firmware ];
+      sessionVariables.ALSA_CONFIG_UCM2 = "${cb-ucm-conf}/share/alsa/ucm2";
+    };
+  };
+}
