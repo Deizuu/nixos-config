59fa3daa357679efe1df43780ed62cb04000c64e
Deizuu <120480615+Deizuu@users.noreply.github.com>
2026-07-05 18:44:43 +0000

    Added back chromebookAudio module

--- hosts/granta.nix
+++ hosts/granta.nix
@@ -6,6 +6,7 @@
       # System
       audioPipewire
       bootGranta
+      chromebookAudio
       desktopGnome
       networking
       printing
---
+++ modules/system/chromebookAudio.nix
@@ -0,0 +1,53 @@
+{ self, inputs, ... }: {
+  flake.nixosModules.chromebookAudio = { config, pkgs, lib, ... }:
+  let
+    cb-ucm-conf = with pkgs; alsa-ucm-conf.overrideAttrs {
+      wttsrc = fetchFromGitHub {
+        owner = "WeirdTreeThing";
+	repo = "alsa-ucm-conf-cros";
+	rev = "6b395ae73ac63407d8a9892fe1290f191eb0315b";
+        hash = "sha256-GHrK85DmiYF6FhEJlYJWy6aP9wtHFKkTohqt114TluI=";
+      };
+      unpackPhase = ''
+        runHook preUnpack
+	tar xf "$src"
+	runHook postUnpack
+      '';
+      installPhase = ''
+        runHook preInstall
+	mkdir -p $out/share/alsa
+	cp -r alsa-ucm*/ucm2 $out/share/alsa
+	runHook postInstall
+      '';
+    };
+  in
+  {
+    environment = {
+      systemPackages = [ pkgs.sof-firmware ];
+      sessionVariables.ALSA_CONFIG_UCM2 = "${cb-ucm-conf}/share/alsa/ucm2";
+      etc = {
+        "wireplumber/main.lua.d/51-increase-headroom.lua".text = ''
+          rule = {
+	    matches = {
+              {
+                { "node.name", "matches", "alsa_output.*" },
+              },
+            },
+            apply_properties = {
+              ["api.alsa.headroom"] = 4096,
+            },
+          }
+
+          table.insert(alsa_monitor.rules,rule)
+        '';
+      };
+    };
+
+    system.replaceRuntimeDependencies = [
+      ({
+        original = pkgs.alsa-ucm-conf;
+	replacement = cb-ucm-conf;
+      })
+    ];
+  };
+}
