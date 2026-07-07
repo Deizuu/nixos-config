f6eb4b87d2681517486ccf4c87192466f5df7ea1
Deizuu <120480615+Deizuu@users.noreply.github.com>
2026-07-07 09:08:52 +0000

    Added Helvum Patchbay

--- modules/programs/proAudio.nix
+++ modules/programs/proAudio.nix
@@ -2,11 +2,12 @@
   flake.homeModules.proAudio = { pkgs, lib, ... }: {
     home.packages = with pkgs; [
       ardour
-      carla
-      neural-amp-modeler-lv2
-      guitarix
       calf
+      carla
+      guitarix
+      helvum
       lsp-plugins
+      neural-amp-modeler-lv2
     ];
   };
 }
