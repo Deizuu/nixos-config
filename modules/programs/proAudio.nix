abf8048321cbda4d1cff60858ea62891acad20ce
Deizuu <120480615+Deizuu@users.noreply.github.com>
2026-07-08 04:20:02 +0000

    Added comments and pitch shifter plugin

--- modules/programs/proAudio.nix
+++ modules/programs/proAudio.nix
@@ -1,13 +1,14 @@
 {
   flake.homeModules.proAudio = { pkgs, lib, ... }: {
     home.packages = with pkgs; [
-      ardour
-      calf
-      carla
-      guitarix
-      helvum
-      lsp-plugins
-      neural-amp-modeler-lv2
+      ardour # DAW
+      calf # Plugin pack
+      carla # Plugin host
+      guitarix # Plugin host & pack
+      helvum # Patchbay
+      lsp-plugins # Plugin pack
+      neural-amp-modeler-lv2 # Plugin
+      vocproc # Plugin
     ];
   };
 }
