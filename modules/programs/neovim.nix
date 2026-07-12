4cbaf369beb3897d86ee533d32e8d43aa2723a4c
Deizuu <120480615+Deizuu@users.noreply.github.com>
2026-07-12 18:01:15 +0200

    enabled vimAlias

--- modules/programs/neovim.nix
+++ modules/programs/neovim.nix
@@ -2,6 +2,7 @@
   flake.homeModules.neovim = { pkgs, ... }: {
     programs.neovim = {
       enable = true;
+      vimAlias = true;
       plugins = with pkgs.vimPlugins; [
         LazyVim
       ];
