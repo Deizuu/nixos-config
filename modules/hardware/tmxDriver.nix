e13ae4fe412c3ec81c6845d5c71bbfb5d0bd1fe7
Deizuu <120480615+Deizuu@users.noreply.github.com>
2026-07-18 19:55:23 +0200

    Added default values at connection to steering wheel

--- modules/hardware/tmxDriver.nix
+++ modules/hardware/tmxDriver.nix
@@ -4,7 +4,11 @@
     boot.kernelModules = [ "hid-t150" ];
 
     services.udev.extraRules = ''
+      # Allow non-root access to the wheel
       ACTION=="add", SUBSYSTEM=="usb", ATTRS{idVendor}=="044f", ATTRS{idProduct}=="b67e", MODE="0666", TAG+="uaccess"
+
+      # Set default range and gain when driver attaches (range -> [ 270; 900 ], gain -> [ 0;65535 ])
+      SUBSYSTEM=="hid", ATTRS{driver}=="hid-t150", ATTR{range}="270", ATTR{gain}="65535"
     '';
   };
 }
