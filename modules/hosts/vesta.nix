0afced45b4e3d89e3b39ae3428aa00e05cbffb08
Deizuu <120480615+Deizuu@users.noreply.github.com>
2026-07-18 16:03:33 +0200

    Set resolution on vesta

--- modules/hosts/vesta.nix
+++ modules/hosts/vesta.nix
@@ -11,6 +11,11 @@
     ];
 
     boot.kernelPackages = pkgs.linuxPackages_6_6;
+    
+    # Low res because of stinky VGA cable
+    services.xserver.resolutions = [
+      { x = 1600; y = 900; }
+    ];
 
     networking.hostName = "vesta";
     system.stateVersion = "26.05";
