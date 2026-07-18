e04ad76abe608d7750a6f20ccc73458c7e03ac52
Deizuu <120480615+Deizuu@users.noreply.github.com>
2026-07-18 23:51:03 +0200

    Fixed syntax error with the outputs and fixed tmx-driver package

--- modules/hardware/thrustmasterTmx/tmx-driver.pkg.nix
+++ modules/hardware/thrustmasterTmx/tmx-driver.pkg.nix
@@ -4,27 +4,29 @@
   linuxHeaders,
   kernel,
   kernelModuleMakeFlags
-}: stdenv.mkDerivation rec {
+}:
+stdenv.mkDerivation rec {
   pname = "tmx-driver";
   version = "unstable";
   
   src = fetchFromGitHub {
     owner = "emtek995";
     repo = "TMX-driver";
-    rev = "7e23d9a388de18b96903b6c3e76b20f3a060091f";  # Specific commit
-    sha256 = "1hcy2rhpa3kzpmd0rssk80pfpq03vlc8annci8lnwnrdl68msc8y";  # Prefetch hash
+    rev = "7e23d9a388de18b96903b6c3e76b20f3a060091f";
+    sha256 = "1hcy2rhpa3kzpmd0rssk80pfpq03vlc8annci8lnwnrdl68msc8y";
   };
   
   nativeBuildInputs = kernel.moduleBuildDependencies;
 
-  sourceRoot = "${src.name}";
-
   makeFlags = kernelModuleMakeFlags ++ [
     "KDIR=${kernel.dev}/lib/modules/${kernel.modDirVersion}/build"
-    "INSTALL_MOD_PATH=${placeholder "out"}"
   ];
 
+  buildPhase = ''
+    make -C ${kernel.dev}/lib/modules/${kernel.modDirVersion}/build M=$(pwd) modules
+  '';
+
   installPhase = ''
-    make -C ${kernel.dev}/lib/modules/${kernel.modDirVersion}/build M=$(pwd) modules_install $makeFlags
+    make -C ${kernel.dev}/lib/modules/${kernel.modDirVersion}/build M=$(pwd) INSTALL_MOD_PATH=$out modules_install
   '';
 }
