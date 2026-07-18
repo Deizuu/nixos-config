9c158456a31f88ed028d86bf8483f20fdabad539
Deizuu <120480615+Deizuu@users.noreply.github.com>
2026-07-18 15:06:04 +0200

    Added git user.name and user.email

--- modules/apps/git.nix
+++ modules/apps/git.nix
@@ -3,6 +3,10 @@
     programs.git = {
       enable = true;
       settings = {
+        user = {
+	  name = "Deizuu";
+	  email = "120480615+Deizuu@users.noreply.github.com";
+	};
         init.defaultBranch = "master";
 	push.default = "current";
 	commit.verbose = true;
