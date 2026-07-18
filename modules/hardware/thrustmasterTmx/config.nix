70ef7eb6288f6ebbff3e759973ffec3e02c5cbaf
Deizuu <120480615+Deizuu@users.noreply.github.com>
2026-07-19 00:10:02 +0200

    Updated idProduct to correspond with the actual idProduct of the Thrustmaster TMX

--- modules/hardware/thrustmasterTmx/config.nix
+++ modules/hardware/thrustmasterTmx/config.nix
@@ -9,10 +9,10 @@
 
     services.udev.extraRules = ''
       # Allow non-root access to the wheel (USB)
-      ACTION=="add", SUBSYSTEM=="usb", ATTRS{idVendor}=="044f", ATTRS{idProduct}=="b67e", MODE="0666", TAG+="uaccess"
+      ACTION=="add", SUBSYSTEM=="usb", ATTRS{idVendor}=="044f", ATTRS{idProduct}=="b67f", MODE="0666", TAG+="uaccess"
 
       # Allow non-root access to the wheel (HID device)
-      SUBSYSTEM=="hidraw", ATTRS{idVendor}=="044f", ATTRS{idProduct}=="b67e", MODE="0666", TAG+="uaccess"
+      SUBSYSTEM=="hidraw", ATTRS{idVendor}=="044f", ATTRS{idProduct}=="b67f", MODE="0666", TAG+="uaccess"
 
       # Set default range and gain when driver attaches (range -> [ 270; 900 ], gain -> [ 0;65535 ])
       SUBSYSTEM=="hid", ATTRS{driver}=="hid-t150", ATTR{range}="900", ATTR{gain}="65535"
