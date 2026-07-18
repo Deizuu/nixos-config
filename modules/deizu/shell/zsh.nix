3cdfd4062baa0cb6e0a547c1fa342ae1d4bbecd9
Deizuu <120480615+Deizuu@users.noreply.github.com>
2026-07-18 15:55:26 +0200

    Set default shell for user to zsh

--- modules/deizu/shell/zsh.nix
+++ modules/deizu/shell/zsh.nix
@@ -1,7 +1,14 @@
-{
+{ config, ... }: {
   my.user.home.base = {
     programs.zsh = {
       enable = true;
+    };
+  };
+
+  nixos.modules.base = { pkgs, ... }: {
+    programs.zsh.enable = true;
+    users.users.${config.my.user.name} = {
+      shell = pkgs.zsh;
     };
   };
 }
