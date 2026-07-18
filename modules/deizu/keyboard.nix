05c30680d307b699a0403a4fab620960da372f4b
Deizuu <120480615+Deizuu@users.noreply.github.com>
2026-07-18 15:55:39 +0200

    Added keyboard definitions

---
+++ modules/deizu/keyboard.nix
@@ -0,0 +1,10 @@
+{
+  my.user.home.base = {
+    home.keyboard = {
+      layout = "us,fr";
+      model = "pc104";
+      options = "grp:alt_shift_toggle";
+      variant = "colemak_dh_ortho,us";
+    };
+  };
+}
