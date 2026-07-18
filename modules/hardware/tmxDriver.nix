98b5cbb268f3aca37e2d72568188de37a8b2ffa7
Deizuu <120480615+Deizuu@users.noreply.github.com>
2026-07-18 19:50:07 +0200

    Updated product ID to match the real product ID

--- modules/hardware/tmxDriver.nix
+++ modules/hardware/tmxDriver.nix
@@ -4,7 +4,7 @@
     boot.kernelModules = [ "hid-t150" ];
 
     services.udev.extraRules = ''
-      ACTION=="add", SUBSYSTEM=="usb", ATTRS{idVendor}=="044f", ATTRS{idProduct}=="b68c", MODE="0666", TAG+="uaccess"
+      ACTION=="add", SUBSYSTEM=="usb", ATTRS{idVendor}=="044f", ATTRS{idProduct}=="b67e", MODE="0666", TAG+="uaccess"
     '';
   };
 }
