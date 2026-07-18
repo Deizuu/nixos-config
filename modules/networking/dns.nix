fd55ffa420d2c854eb45e9eb92624ba7c211dc25
Deizuu <120480615+Deizuu@users.noreply.github.com>
2026-07-18 19:41:17 +0200

    Attempt fixing DNS not being set

--- modules/networking/dns.nix
+++ modules/networking/dns.nix
@@ -1,16 +1,26 @@
 {
   nixos.modules.base = {
+    systemd.network.networks."10-no-dhcp-dns" = {
+      matchConfig = {
+        Name = "eno2";
+      };
+
+      dhcpV4Config.UseDNS = false;
+      ipv6AcceptRAConfig.UseDNS = false;
+
+      # do you use dhcpv6?
+      # dhcpV6Config.UseDNS = false;
+
+      # copy-pasted from https://github.com/NixOS/nixpkgs/blob/140e28cf6e76915d9440e06facdb5e1e4f70a342/nixos/modules/tasks/network-interfaces-systemd.nix#L61-L68
+      DHCP = "yes";
+      networkConfig.IPv6PrivacyExtensions = "kernel";
+    };
     services.resolved = {
       enable = true;
       settings.Resolve = {
         DNSOverTls = "opportunistic";
 	DNS = [
-          #"194.242.2.2"#dns.mullvad.net
-          #"194.242.2.3"#adblock.dns.mullvad.net
-          #"194.242.2.4"#base.dns.mullvad.net
-          #"194.242.2.5"#extended.dns.mullvad.net
           "194.242.2.6"#family.dns.mullvad.net
-          #"194.242.2.9"#all.dns.mullvad.net
 	];
       };
     };
