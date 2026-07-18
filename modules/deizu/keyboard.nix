f6e7ee67b2b15a79220e27890e9e7057ca18bcec
Deizuu <120480615+Deizuu@users.noreply.github.com>
2026-07-18 16:40:42 +0200

    Set user-specific keyboard layout

--- modules/deizu/keyboard.nix
+++ modules/deizu/keyboard.nix
@@ -1,10 +1,19 @@
 {
-  my.user.home.base = {
+  my.user.home.base = { pkgs, lib, ... }: {
     home.keyboard = {
       layout = "us,fr";
-      model = "pc104";
-      options = "grp:alt_shift_toggle";
-      variant = "colemak_dh_ortho,us";
+      variant = "colemak_dh_ortho_,us";
+      options = [ "grp:alt_shift_toggle" ];
+    };
+
+    dconf.settings = {
+      "org/cinnamon/desktop/input-sources" = {
+        sources = [
+          (lib.hm.gvariant.mkTuple [ "xkb" "us+colemak_dh_ortho" ])
+          (lib.hm.gvariant.mkTuple [ "xkb" "fr" ])
+        ];
+        xkb-options = ["grp:alt_shift_toggle"];
+      };
     };
   };
 }
