ff75373299a8fe263ee66f8514d9200c82799310
Deizuu <120480615+Deizuu@users.noreply.github.com>
2026-07-20 15:19:37 +0200

    Removed resolution setting

--- modules/hosts/vesta.nix
+++ modules/hosts/vesta.nix
@@ -12,11 +12,6 @@
     ];
 
     boot.kernelPackages = pkgs.linuxPackages_6_6;
-    
-    # Low res because of stinky VGA cable
-    services.xserver.resolutions = [
-      { x = 1600; y = 900; }
-    ];
 
     networking.hostName = "vesta";
     system.stateVersion = "26.05";
