6b01af74a677f3bbbaa09751ea96a7577c27d007
Deizuu <120480615+Deizuu@users.noreply.github.com>
2026-07-23 00:12:00 +0200

    tabstop & shiftwidth -> modules/deizu/neovim/indent.nix

--- modules/deizu/neovim/indent.nix
+++ modules/deizu/neovim/indent.nix
@@ -2,6 +2,8 @@
   my.user.home.base = {
     programs.nvf.settings.vim = {
       options.autoindent = true;
+      options.tabstop = 2;
+      options.shiftwidth = 2;
     };
   };
 }
--- modules/deizu/neovim/options.nix
+++
@@ -1,8 +0,0 @@
-{
-  my.user.home.base = {
-    programs.nvf.settings.vim = {
-      options.tabstop = 2;
-      options.shiftwidth = 2;
-    };
-  };
-}
