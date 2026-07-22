461b81499d3c6778f794e46d7c7dd3db0b28e241
Deizuu <120480615+Deizuu@users.noreply.github.com>
2026-07-22 16:19:13 +0200

    Disabled TPM for granta

--- modules/hosts/granta.nix
+++ modules/hosts/granta.nix
@@ -9,6 +9,10 @@
       gdm
       gnome
     ];
+    
+    # Disable TPM2 to fix boot waiting time
+    boot.initrd.systemd.tpm2.enable = true;
+    systemd.tpm2.enable = true;
 
     networking.hostName = "granta";
     system.stateVersion = "26.05";
