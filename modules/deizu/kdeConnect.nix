99f22acb573adf60c8c3b5aa4b2538eca74bdbd4
Deizuu <120480615+Deizuu@users.noreply.github.com>
2026-07-22 22:48:29 +0200

    Added KDE Connect for Deizu

---
+++ modules/deizu/kdeConnect.nix
@@ -0,0 +1,13 @@
+{
+  my.user.home.gui = {
+    services.kdeconnect = {
+      enable = true;
+      indicator = true;
+    };
+
+    networking.firewall = rec {
+      allowedTCPPortRanges = [ { from = 1714; to = 1764; } ];
+      allowedUDPPortRanges = allowedTCPPortRanges;
+    };
+  };
+}
