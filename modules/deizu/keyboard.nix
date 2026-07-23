488b1b92afda6d5514650f60d8d6fa45ffdc3848
Deizuu <120480615+Deizuu@users.noreply.github.com>
2026-07-23 12:08:08 +0200

    Removed modules/deizu/keyboard.nix (needed layout is QWERTY anyway)

--- modules/deizu/keyboard.nix
+++
@@ -1,9 +0,0 @@
-{
-  my.user.home.base = { pkgs, lib, ... }: {
-    home.keyboard = {
-      layout = "us";
-      variant = "colemak_dh_ortho";
-      options = [ "grp:alt_shift_toggle" ];
-    };
-  };
-}
