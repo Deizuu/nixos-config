e7857578a06491bd49bbcc12411dfd60b1403ced
Deizuu <120480615+Deizuu@users.noreply.github.com>
2026-07-24 20:05:41 +0200

    starship: updated configuration for deizu

--- modules/deizu/shell/starship.nix
+++ modules/deizu/shell/starship.nix
@@ -1,8 +1,28 @@
 {
-  my.user.home.base = {
+  my.user.home.base = { lib, ... }: {
     programs.starship = {
       enable = true;
       enableZshIntegration = true;
+      settings = {
+        add_newline = false;
+
+        format = lib.concatStrings [
+          "\n"
+          "$all"
+          "$status"
+          "$shlvl"
+          "$character"
+        ];
+        direnv.disabled = false;
+        status = {
+          disabled = false;
+          format = "[$symbol]($style)";
+        };
+        shlvl.disabled = false;
+        username = {
+          show_always = true;
+        };
+      };
     };
   };
 }
