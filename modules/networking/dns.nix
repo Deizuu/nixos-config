9b9ae9ea779cff49b287a2d2304a9d19647fc21b
Deizuu <120480615+Deizuu@users.noreply.github.com>
2026-08-02 14:21:27 +0200

    Fixed brain fart

--- modules/networking/dns.nix
+++ modules/networking/dns.nix
@@ -5,7 +5,7 @@
       useDHCP = false;
       dhcpcd.enable = false;
 
-      networking.nameservers = [
+      nameservers = [
         "194.242.2.6" # family.dns.mullvad.net
         "9.9.9.9" # Quad9 DNS (fallback)
         "1.1.1.1" # Cloudflare DNS (most fallbackest of the fallback)
