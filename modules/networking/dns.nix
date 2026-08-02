ed3c35454711e4bbef48f0e6d89e0748775dc0c4
Deizuu <120480615+Deizuu@users.noreply.github.com>
2026-08-02 16:03:03 +0200

    Removed conflicting networkmanager.dns declaration, and changed mullvad dns

--- modules/networking/dns.nix
+++ modules/networking/dns.nix
@@ -1,13 +1,11 @@
 {
   nixos.modules.base = {
     networking = {
-      networkmanager.dns = "none";
       useDHCP = false;
       dhcpcd.enable = false;
 
       nameservers = [
-        "194.242.2.2" # dns.mullvad.net
-        #"194.242.2.6" # family.dns.mullvad.net
+        "194.242.2.6" # family.dns.mullvad.net
         "9.9.9.9" # Quad9 DNS (fallback)
         "1.1.1.1" # Cloudflare DNS (most fallbackest of the fallback)
       ];
