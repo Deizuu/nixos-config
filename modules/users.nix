9078ccde173cc6438c51e1ca82a833cde488b32a
Deizuu <120480615+Deizuu@users.noreply.github.com>
2026-08-27 10:34:57 +0200

    Fixed config issue with modules/users.nix

--- modules/users.nix
+++ modules/users.nix
@@ -3,46 +3,51 @@
     { config, lib, ... }:
     {
       options.dzu.users = lib.mkOption {
-        type = lib.types.lazyAttrsOf lib.types.lazyAttrsOf lib.types.submodule (
-          { config, ... }:
-          {
-            options = {
-              name = lib.mkOption {
-                type = lib.types.str;
-                description = "User's name";
+        type = lib.types.lazyAttrsOf lib.types.submodule ({
+          options = {
+            name = lib.mkOption {
+              type = lib.types.str;
+              description = "User's name";
+            };
+            description = lib.mkOption {
+              type = lib.types.str;
+              description = "User's full name";
+            };
+            extraGroups = lib.mkOption {
+              type = lib.types.listOf lib.types.str;
+              description = "Groups the user is apart of";
+            };
+            module = lib.mkOption {
+              type = lib.types.deferredModuleWith {
+                imports = with self.modules.hm; [
+                  base
+                  gui
+                ];
               };
-              description = lib.mkOption {
-                type = lib.types.str;
-                description = "User's full name";
-              };
-              extraGroups = lib.mkOption {
-                type = lib.types.listOf lib.types.str;
-                description = "Groups the user is apart of";
-              };
-              module = lib.mkOption {
-                type = lib.types.deferredModuleWith {
-                  imports = with self.modules.hm; [
-                    base
-                    gui
-                  ];
-                };
-                description = "User's HM module";
-              };
+              description = "User's HM module";
+            };
+          };
+
+          config = {
+            users.users.${config.name} = {
+              isNormalUser = true;
+              inherit (config) description;
+              extraGroups = lib.mkMerge [
+                config.extraGroups
+                config.dzu.usersDefaultGroups
+              ];
             };
 
-            config = {
-              users.users.${config.name} = {
-                isNormalUser = true;
-                inherit (config) description extraGroups;
-              };
-
-              home-manager.users.${config.name} = {
-                imports = [ config.module ];
-              };
+            home-manager.users.${config.name} = {
+              imports = [ config.module ];
             };
-          }
-        );
+          };
+        });
         description = "Attrset containing user submodules";
+      };
+      options.dzu.usersDefaultGroups = lib.mkOption {
+        type = lib.types.listOf lib.types.str;
+        description = "Default groups for all users";
       };
     };
 }
