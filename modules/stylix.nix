d63ede391c74f58b3adcc0acc9b43722c4627cb2
Deizuu <120480615+Deizuu@users.noreply.github.com>
2026-08-29 18:38:27 +0200

    Fixed stylix module

--- modules/stylix.nix
+++ modules/stylix.nix
@@ -16,8 +16,8 @@
 
       stylix = {
         enable = lib.mkDefault false;
-        base16Scheme = lib.mkDefault "${pkgs.base16-schemes}/share/themes/catpuccin-macchiato.yaml";
-        image = lib.mkDefault pkgs.fetchurl {
+        base16Scheme = lib.mkDefault "${pkgs.base16-schemes}/share/themes/catppuccin-macchiato.yaml";
+        image = pkgs.fetchurl {
           url = "https://getwallpapers.com/wallpaper/full/f/b/3/6032.jpg"; # TODO change wallpaper, this is a temp
           hash = "sha256-NdtooA+oODFnEdUeEwxlErVnFICt8JSzc9IMa8xLFxI=";
         };
