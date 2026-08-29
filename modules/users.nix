4983ba1a4e218d9de3f8414f5decd7e867f4b21b
Deizuu <120480615+Deizuu@users.noreply.github.com>
2026-08-29 19:22:15 +0200

    something something, useful commit message, blablabla

--- modules/users.nix
+++ modules/users.nix
@@ -3,47 +3,49 @@
     { config, lib, ... }:
     {
       options.dzu.users = lib.mkOption {
-        type = lib.types.lazyAttrsOf lib.types.submodule ({
-          options = {
-            enable = lib.mkEnableOption "Enable user";
-            name = lib.mkOption {
-              type = lib.types.str;
-              description = "User's name";
+        type = lib.types.lazyAttrsOf lib.types.submodule (
+          { config, ... }: {
+            options = {
+              enable = lib.mkEnableOption "Enable user";
+              name = lib.mkOption {
+                type = lib.types.str;
+                description = "User's name";
+              };
+              description = lib.mkOption {
+                type = lib.types.str;
+                description = "User's full name";
+              };
+              extraGroups = lib.mkOption {
+                type = lib.types.listOf lib.types.str;
+                description = "Groups the user is apart of";
+              };
+              module = lib.mkOption {
+                type = lib.types.deferredModuleWith {
+                  imports = with self.modules.hm; [
+                    base
+                    gui
+                  ];
+                };
+                description = "User's HM module";
+              };
             };
-            description = lib.mkOption {
-              type = lib.types.str;
-              description = "User's full name";
-            };
-            extraGroups = lib.mkOption {
-              type = lib.types.listOf lib.types.str;
-              description = "Groups the user is apart of";
-            };
-            module = lib.mkOption {
-              type = lib.types.deferredModuleWith {
-                imports = with self.modules.hm; [
-                  base
-                  gui
+
+            config = {
+              users.users.${config.name} = {
+                isNormalUser = true;
+                inherit (config) description;
+                extraGroups = lib.mkMerge [
+                  config.extraGroups
+                  config.dzu.usersDefaultGroups
                 ];
               };
-              description = "User's HM module";
-            };
-          };
 
-          config = {
-            users.users.${config.name} = {
-              isNormalUser = true;
-              inherit (config) description;
-              extraGroups = lib.mkMerge [
-                config.extraGroups
-                config.dzu.usersDefaultGroups
-              ];
+              home-manager.users.${config.name} = {
+                imports = [ config.module ];
+              };
             };
-
-            home-manager.users.${config.name} = {
-              imports = [ config.module ];
-            };
-          };
-        });
+          }
+        );
         description = "Attrset containing user submodules";
       };
       options.dzu.usersDefaultGroups = lib.mkOption {
