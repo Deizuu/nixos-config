803956315ec05fe6b09e0c7fc06488a580a0fca5
Deizuu <120480615+Deizuu@users.noreply.github.com>
2026-08-30 02:14:15 +0200

    Added program options for deizu

--- modules/users/deizu.nix
+++ modules/users/deizu.nix
@@ -1,5 +1,5 @@
 { self, ... }: {
-  flake.modules.nixos.users = {
+  flake.modules.nixos.base = {
     dzu.users.deizu = {
       name = "deizu";
       description = "Deizu";
@@ -10,10 +10,44 @@
         ];
       };
     };
+    dzu.allowUnfreePackages = [
+      "clonehero"
+      "osu-lazer-bin"
+    ];
   };
-  flake.modules.hm.deizu = {
+  flake.modules.hm.deizu = { pkgs, ... }: {
     home.sessionVariables = {
       SSH_AUTH_SOCK = "/home/deizu/.var/app/com.bitwarden.desktop/data/.bitwarden-ssh-agent.sock"; # TODO find a purer way to set this (maybe imperatively after installing Bitwarden Desktop?)
+    };
+
+    dzu.apps = {
+      blender.enable = true;
+      distrobox.enable = true;
+      element.enable = true;
+      ente.enable = true;
+      gimp.enable = true;
+      github-cli.enable = true;
+      kdeconnect.enable = true;
+      kdenlive.enable = true;
+      neovim = {
+        enable = true;
+        defaultEditor = true;
+      };
+      opencode.enable = true;
+      proAudio.enable = true;
+      rclone.enable = true;
+      ripgrep.enable = true;
+      signal.enable = true;
+      zen-browser.enable = true;
+      zennotes.enable = true;
+
+      games = {
+        enable = true;
+        games = with pkgs; [
+          clonehero
+          osu-lazer-bin
+        ];
+      };
     };
   };
 }
