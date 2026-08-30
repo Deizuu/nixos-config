ac45b6c87d21f08ce44ef393f548d0ebe8035d93
Deizuu <120480615+Deizuu@users.noreply.github.com>
2026-08-30 02:20:35 +0200

    Added vesta

---
+++ modules/hosts/vesta.nix
@@ -0,0 +1,53 @@
+{ self, inputs, ... }: {
+  flake.nixosConfigurations.vesta = inputs.nixpkgs.lib.nixosSystem {
+    modules = with self.modules.nixos; [
+      vesta
+      vestaHardware
+
+      pc
+    ];
+  };
+
+  flake.modules.nixos.vesta = {
+    dzu.boot.silentBoot = true;
+    dzu.boot.usePlymouth = true;
+
+    dzu.audio = {
+      enable = true;
+      enableChromebook = true;
+      enableJack = true;
+    };
+
+    dzu.hw.nvidiaKepler.enable = true;
+
+    dzu.autoTime.enable = true;
+    dzu.location.enable = true;
+
+    dzu.login.lightdm.enable = true;
+    dzu.de.xfce.enable = true;
+
+    dzu.networking = {
+      wifi.enable = true;
+      discovery.enable = true;
+    };
+
+    dzu.printing.enable = true;
+
+    dzu.users.deizu.enable = true;
+
+    dzu.apps = {
+      file-roller.enable = true;
+      gamemode.enable = true;
+      hotspot.enable = true;
+      oversteer.enable = true;
+      polychromatic.enable = true;
+      steam.enable = true;
+    };
+
+    musnix.enable = true;
+    stylix.enable = true;
+
+    networking.hostName = "vesta";
+    system.stateVersion = "26.05";
+  };
+}
