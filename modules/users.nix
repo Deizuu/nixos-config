c04cbcc108de2e8dbfa0f5d92b694e0f4e12cae1
Deizuu <120480615+Deizuu@users.noreply.github.com>
2026-08-30 23:54:40 +0200

    Overhauled & fixed users module

--- modules/users.nix
+++ modules/users.nix
@@ -1,4 +1,4 @@
-{ self, ... }: {
+{
   flake.modules.nixos.base =
     {
       config,
@@ -6,56 +6,41 @@
       pkgs,
       ...
     }:
+    let
+      cfg = config.dzu.users;
+      userSubmodule = lib.types.submodule (
+        { name, ... }: {
+          options = {
+            enable = lib.mkEnableOption "Enable user";
+            name = lib.mkOption {
+              type = lib.types.str;
+              default = name;
+              description = "User name";
+            };
+            description = lib.mkOption {
+              type = lib.types.str;
+              description = "User's full name";
+            };
+            extraGroups = lib.mkOption {
+              type = lib.types.listOf lib.types.str;
+              description = "Groups the user is apart of";
+            };
+            shell = lib.mkOption {
+              type = lib.types.package;
+              description = "User shell";
+              default = pkgs.zsh;
+            };
+            module = lib.mkOption {
+              type = lib.types.deferredModule;
+              description = "User's HM module";
+            };
+          };
+        }
+      );
+    in
     {
       options.dzu.users = lib.mkOption {
-        type = lib.types.lazyAttrsOf lib.types.submodule (
-          { config, ... }: {
-            options = {
-              enable = lib.mkEnableOption "Enable user";
-              name = lib.mkOption {
-                type = lib.types.str;
-                description = "User's name";
-              };
-              description = lib.mkOption {
-                type = lib.types.str;
-                description = "User's full name";
-              };
-              extraGroups = lib.mkOption {
-                type = lib.types.listOf lib.types.str;
-                description = "Groups the user is apart of";
-              };
-              shell = lib.mkOption {
-                type = lib.types.package;
-                description = "User shell";
-                default = pkgs.zsh;
-              };
-              module = lib.mkOption {
-                type = lib.types.deferredModuleWith {
-                  imports = with self.modules.hm; [
-                    base
-                    gui
-                  ];
-                };
-                description = "User's HM module";
-              };
-            };
-
-            config = {
-              users.users.${config.name} = {
-                isNormalUser = true;
-                inherit (config) description shell;
-                extraGroups = lib.mkMerge [
-                  config.extraGroups
-                  config.dzu.usersDefaultGroups
-                ];
-              };
-
-              home-manager.users.${config.name} = {
-                imports = [ config.module ];
-              };
-            };
-          }
-        );
+        type = lib.types.attrsOf userSubmodule;
         description = "Attrset containing user submodules";
       };
       options.dzu.usersDefaultGroups = lib.mkOption {
@@ -63,11 +48,40 @@
         description = "Default groups for all users";
       };
       config = {
+        users.users = lib.mapAttrs' (name: user: {
+          name = "${name}";
+          value = {
+            enable = user.enable;
+            isNormalUser = user.enable;
+            inherit (user) description shell;
+            extraGroups = lib.mkMerge [
+              config.dzu.usersDefaultGroups
+              user.extraGroups
+            ];
+          };
+        }) cfg;
         dzu.usersDefaultGroups = [
           "networkmanager"
           "input"
           "openrazer"
         ];
+        programs.zsh.enable = true;
       };
     };
 }
+
+#   config = {
+#     users.users.${config.name} = {
+#       isNormalUser = true;
+#       inherit (config) description shell;
+#       extraGroups = lib.mkMerge [
+#         config.extraGroups
+#         config.dzu.usersDefaultGroups
+#       ];
+#     };
+#
+#     home-manager.users.${config.name} = {
+#       imports = [ config.module ];
+#     };
+#   };
+# }
