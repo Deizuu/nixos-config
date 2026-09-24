07c9802b051ff45d567007ebec3d3997d7186ab9
Deizuu <120480615+Deizuu@users.noreply.github.com>
2026-09-25 00:54:40 +0200

    fixed brain fart

--- modules/hosts/granta.nix
+++ modules/hosts/granta.nix
@@ -30,7 +30,7 @@
     dzu.networking = {
       wifi.enable = true;
       discovery.enable = true;
-      granta.enable = true;
+      tailscale.enable = true;
     };
 
     dzu.printing.enable = true;
